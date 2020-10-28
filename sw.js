b7f9ca593aef26f4b795a6704b33b15e7850f082
Chess Kuo <gyc880323@gmail.com>
2020-10-28 17:20:21 +0800

    sw ok

--- sw.js
+++ sw.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const CACHE_NAME = 'pwa-test-v1';
+const CACHE_NAME = 'cache-test-v1';
 const CACHE_FILES = [
-	'sheep.mp4',
+	'/',
+	'/index.html',
+	'/offline.html',
+	'/img/icon/icon.rev.nobg.png',
+	'/css/bootstrap.min.css',
+	'/js/jquery-3.5.1.slim.min.js',
+	'/mural.json',
 ];
 
 self.addEventListener('install', e => {
@@ -30,12 +36,17 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-	console.log('[sw] fetch:', e);
+	console.log('[sw] fetch:', e.request.url);
 	e.respondWith(
-		caches.match(e.request)
+		caches.match(e.request.url)
 			.then(res => {
 				if(res) return res;
 				else return fetch(e.request);
 			})
-		);
+			.catch(err => {
+				if(e.request.url.includes('ar.html')){
+					return caches.match('/offline.html');
+				}
+			})
+	);
 });
